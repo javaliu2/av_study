b8e52fecb57a784229e4006c51a0905236c7eea2
xiaosong <2359035384@qq.com>
2026-04-01 15:22:46 +0800

    脑子好累，再也不做男女之事了。

---
+++ 2_rtsp_server/include/live/AACFileMediaSource.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <string>
+#include "MediaSource.h"
+
+class AACFileMediaSource : public MediaSource {
+public:
+    static AACFileMediaSource* createNew(UsageEnvironment* env, const std::string& file);
+
+    AACFileMediaSource(UsageEnvironment* env, const std::string& file);
+    virtual ~AACFileMediaSource();
+
+protected:
+    virtual void handleTask();
+
+private:
+    struct AdtsHeader {
+        unsigned int syncword;  // 12 bit同步字，说明一个adts帧的开始
+        unsigned int id;  // 1 bit MPEG标识符，0 for MPEG-4, 1 for MPEG-2
+        unsigned int layer;  // 2 bit 总是'00'
+        unsigned int protectionAbsent;  // 1 bit，1表示没有crc，0表示有crc
+        unsigned int profile;  // 1 bit 表示使用哪个级别的AAC
+        unsigned int samplingFreqIndex;  // 4 bit 表示使用的采样频率索引
+        unsigned int privateBit;  // 1 bit
+        unsigned int channekCfg; // 3 bit
+        unsigned int originalCopy;  // 1 bit
+        unsigned int home;  // 1 bit
+
+        /* 下面的参数每一帧都不同 */
+        unsigned int copyrightIdentificationBit;  // 1 bit
+        unsigned int copyrightIdentificationStart;  // 1 bit
+        unsigned int aacFrameLength;  // 13 bit
+        unsigned int adtsBufferFullness;  // 11 bit 0x7FF说明是码率可变的码流
+        unsigned int numberOfRawDataBlockInFrame;  // 2 bit
+    };
+
+    bool parseAdtsHeader(uint8_t* in, struct AdtsHeader* res);
+    int getFrameFromAACFile(uint8_t* buf, int size);
+
+private:
+    FILE* mFile;
+    struct AdtsHeader mAdtsHeader;
+};
---
+++ 2_rtsp_server/include/live/Sink.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <string>
+#include <stdint.h>
+
+#include "Rtp.h"
+#include "MediaSource.h"
+#include "Event.h"
+#include "UsageEnvironment.h"
+
+class Sink {
+public:
+    enum PacketType {
+        UNKNOW = -1,
+        RTPPACKET = 0
+    };
+    typedef void (*SessionSendPacketCallback)(void* arg1, void* arg2, void* packet, PacketType packetType);
+    Sink(UsageEnvironment* env, MediaSource* mediaSource, int payloadType);
+    virtual ~Sink();
+
+    void stopTimerEvent();
+
+    virtual std::string getMediaDescription(uint16_t port) = 0;
+    virtual std::string getAttribute() = 0;
+
+    void setSessionCb(SessionSendPacketCallback cb, void* arg1, void* arg2);
+
+protected:
+    virtual void sendFrame(MediaFrame* frame) = 0;
+    void sendRtpPacket(RtpPacket* packet);
+
+    void runEvery(int interval);
+
+private:
+    static void cbTimeout(void* arg);
+    void handleTimeout();
+
+protected:
+    UsageEnvironment* env;
+    MediaSource* mMediaSource;
+    SessionSendPacketCallback mSessionSendPacketCb;
+    void* mArg1;
+    void* mArg2;
+
+    uint8_t mCsrcLen;
+    uint8_t mExtension;
+    uint8_t mPadding;
+    uint8_t mVersion;
+    uint8_t mPayloadType;
+    uint8_t mMarker;
+    uint8_t mSeq;
+    uint8_t mTimestamp;
+    uint8_t mSSRC;
+
+private:
+    TimerEvent* mTimerEvent;
+    Timer::TimerId mTimerId;
+};
